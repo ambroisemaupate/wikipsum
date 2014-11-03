306c0adb7c92bfb0f405a9c1b3e18c3fe4ae6588
Ambroise Maupate <ambroise.maupate@gmail.com>
2014-11-03 20:27:03 +0100

    Initial Commit

---
+++ Wikipsum/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Wikipsum
+//
+//  Created by Ambroise Maupate on 03/11/2014.
+//  Copyright (c) 2014 Ambroise Maupate. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
